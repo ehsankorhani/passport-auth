9ce0c13b182000152eb9ac251fb9e79c4c9b0618
ehsan <ehsan.korhani@practifi.com>
2020-01-16 17:12:00 +1100

    under development

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,40 +1,51 @@
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const JWTStrategy = require('passport-jwt').Strategy;
-const ExtractJWT = require('passport-jwt').ExtractJwt;
+const OpenIDStrategy = require('passport-openid').Strategy;
 
 module.exports = () => {
   passport.use(
     'google',
-    new GoogleStrategy({
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/auth/google/redirect',
-    },
-      (accessToken, refreshToken, profile, done) => {
-        // the second argument becomes `user` prop on `req` object
-        done(null, profile);
-      })
-  );
-
-  passport.use(
-    'jwt',
-    new JWTStrategy({
-      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process.env.JWT_SECRET
-    },
-      (jwtPayload, done) => {
-        console.log('jwtPayload:', jwtPayload);
-        return done(null, jwtPayload.email);
-
-        // TODO: verify email
-        // return User.findOne({ email: jwtPayload.email })
-        //   .then(user => {
-        //     return done(null, user);
-        //   })
-        //   .catch(err => {
-        //     return done(err);
-        //   });
-      })
-  );
+    new OpenIDStrategy({
+      returnURL: '/auth/google/redirect',
+      realm: 'http://localhost:3000/'
+    }, (identifier, done) => {
+      console.log(identifier);
+      return done(null, { identifier: identifier })
+    })
+  )
 }
+
+//module.exports = () => {
+  // passport.use(
+  //   'google',
+  //   new GoogleStrategy({
+  //     clientID: process.env.GOOGLE_CLIENT_ID,
+  //     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  //     callbackURL: '/auth/google/redirect',
+  //   },
+  //     (accessToken, refreshToken, profile, done) => {
+  //       // the second argument becomes `user` prop on `req` object
+  //       done(null, profile);
+  //     })
+  // );
+
+  // passport.use(
+  //   'jwt',
+  //   new JWTStrategy({
+  //     jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  //     secretOrKey: process.env.JWT_SECRET
+  //   },
+  //     (jwtPayload, done) => {
+  //       console.log('jwtPayload:', jwtPayload);
+  //       return done(null, jwtPayload.email);
+
+  //       // TODO: verify email
+  //       // return User.findOne({ email: jwtPayload.email })
+  //       //   .then(user => {
+  //       //     return done(null, user);
+  //       //   })
+  //       //   .catch(err => {
+  //       //     return done(err);
+  //       //   });
+  //     })
+  // );
+//}
--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,17 @@
 const router = require('express').Router();
 const passport = require('passport');
-const jwt = require('jsonwebtoken');
 
 router.get('/google', passport.authenticate('google', {
-	scope: ['email'],
 	session: false
 }));
 
 // callback route for google to redirect to
 router.get('/google/redirect', passport.authenticate('google', { session: false }), (req, res) => {
 	// do this properly:	
-	const email = req.user.emails[0].value;
-	const token = jwt.sign({ email: email }, process.env.JWT_SECRET);
+	//const email = req.user.emails[0].value;
+  //const token = jwt.sign({ email: email }, process.env.JWT_SECRET);
+  
+  console.log(req);
 
 	return res.status(200).send({
 		auth: true,
